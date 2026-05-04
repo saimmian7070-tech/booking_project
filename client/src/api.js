d16deff51829aef60fe50a331043ff7dc8509787
SaimIshfaq <140852054+SaimIshfaq@users.noreply.github.com>
2026-05-04 11:45:44 +0500

    new change made 1

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bookingproject-production-a88d.up.railway.app/",
+  baseURL: "process.env.REACT_APP_API_URL",
 });
 
 API.interceptors.request.use((req) => {
